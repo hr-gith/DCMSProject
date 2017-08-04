ef069518ac00042b01d23c370ad11917b9002849
hr-gith <hamideh.riazat@gmail.com>
2017-08-03 21:29:25 -0400

    add start method to starting servers from replica manger

--- src/replicaManagement/ReplicaManager.java
+++ src/replicaManagement/ReplicaManager.java
@@ -2,10 +2,14 @@ package replicaManagement;
 
 import org.omg.CORBA.ORB;
 
+import replica1.servers.CenterServers;
+
 public abstract class ReplicaManager {
 	public String id;
 	public boolean leaderStatus;
 	
-	
+	public void start(){
+		CenterServers.main(null);
+	};
 	
 }
